afe66b441ee738d60faf262a1d69a140174e94ba
uwanitheekshani <uwanitheekshani@gmail.com>
2022-05-16 12:20:40 +0530

    Content Updated with DAOFactory

--- Layered_Architecture/src/bo/custom/impl/CustomerBOImpl.java
+++ Layered_Architecture/src/bo/custom/impl/CustomerBOImpl.java
@@ -1,6 +1,7 @@
 package bo.custom.impl;
 
 import bo.custom.CustomerBO;
+import dao.DAOFactory;
 import dao.custom.CustomerDAO;
 import dao.custom.impl.CustomerDAOImpl;
 import model.CustomerDTO;
@@ -9,7 +10,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CustomerBOImpl implements CustomerBO {
-    private final CustomerDAO customerDAO = new CustomerDAOImpl();
+    private final CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+
+   // private final CustomerDAO customerDAO = new CustomerDAOImpl();
 
     @Override
     public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
--- Layered_Architecture/src/bo/custom/impl/PurchaseOrderBOImpl.java
+++ Layered_Architecture/src/bo/custom/impl/PurchaseOrderBOImpl.java
@@ -1,6 +1,8 @@
 package bo.custom.impl;
 
 import bo.custom.PurchaseOrderBO;
+import dao.DAOFactory;
+import dao.SuperDAO;
 import dao.custom.*;
 import dao.custom.impl.*;
 import db.DBConnection;
@@ -17,11 +19,18 @@ import java.util.List;
 
 public class PurchaseOrderBOImpl implements PurchaseOrderBO {
 
-    private final CustomerDAO customerDAO = new CustomerDAOImpl();
-    private final ItemDAO itemDAO = new ItemDAOImpl();
-    private final OrderDAO orderDAO = new OrderDAOImpl();
-    private final OrderDetailsDAO orderDetailsDAO = new OrderDetailsDAOImpl();
-    private final QueryDAO queryDAO = new QueryDAOImpl();
+    private final CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+    private final ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+    private final OrderDAO orderDAO = (OrderDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDER);
+    private final OrderDetailsDAO orderDetailsDAO = (OrderDetailsDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDERDETAILS);
+    private final QueryDAO queryDAO = (QueryDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.QUERYDAO);
+
+
+//    private final CustomerDAO customerDAO = new CustomerDAOImpl();
+//    private final ItemDAO itemDAO = new ItemDAOImpl();
+//    private final OrderDAO orderDAO = new OrderDAOImpl();
+//    private final OrderDetailsDAO orderDetailsDAO = new OrderDetailsDAOImpl();
+//    private final QueryDAO queryDAO = new QueryDAOImpl();
 
     @Override
     public boolean purchaseOrder(String orderId, LocalDate orderDate, String customerId, List<OrderDetailDTO> orderDetails) throws SQLException, ClassNotFoundException {
